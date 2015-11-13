0b6308d71e9dd32605fc375074960a0cfc263060
Ricardo da Silva Lima <rslima@senado.gov.br>
2015-11-13 17:06:35 -0200

    Criando encoder para User

--- src/main/scala/ricash/RicashApp.scala
+++ src/main/scala/ricash/RicashApp.scala
@@ -3,12 +3,16 @@ package ricash
 import org.http4s.dsl._
 import org.http4s.server.HttpService
 import org.http4s.server.jetty.JettyBuilder
+import org.http4s.argonaut._
 import ricash.users.UserQueries
+import ricash.users.User
 
 /**
   * Created by rslima on 09/11/15.
   */
 object RicashApp {
+
+  implicit lazy val userEntityEncoder = jsonEncoderOf[List[User]]
 
   def main (args: Array[String]) {
     JettyBuilder.
--- src/main/scala/ricash/users/UserQueries.scala
+++ src/main/scala/ricash/users/UserQueries.scala
@@ -13,6 +13,6 @@ object UserQueries {
 
   val xa = DriverManagerTransactor[Task]("org.postgresql.Driver","jdbc:postgresql://localhost/ricash","postgres","esiq188")
 
-  val allUsers = sql"select * from ricash.tbl_user".query[User].process.transact(xa).map(_.asJson.spaces4)
+  val allUsers = sql"select * from ricash.tbl_user".query[User].list.transact(xa)
 
 }
